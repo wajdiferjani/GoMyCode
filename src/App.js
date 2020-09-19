b44690b8ac644efeb3848c2bdcbf9da9a78df223
wajdiferjani <wajdi.ferjani@esprit.tn>
2020-09-19 10:20:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Form from './Components/Form'
+import Navbar from './Components/Navbar'
+
+
+
+const App = () =>{
+
+
+    return(
+<>
+<Navbar/>
+<Form />
+</>
+    )
+}
+
+
+export default App
+
+
---
+++ src/Components/Form.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Form = () =>{
+    return(
+        <>
+        <form>
+  <div className="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" />
+  </div>
+  <div className="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        </>
+    )
+
+}
+
+
+
+export default Form 
